b14877be5f58762b34592b4f1e2453076b12b0e5
Dave Moten <davidmoten@gmail.com>
2014-04-29 19:12:25 +1000

    dev

--- src/main/java/com/github/davidmoten/maven/MojoExecutorBuilder.java
+++ src/main/java/com/github/davidmoten/maven/MojoExecutorBuilder.java
@@ -1,10 +1,18 @@
 package com.github.davidmoten.maven;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.util.List;
+
+import javassist.bytecode.AnnotationsAttribute;
+import javassist.bytecode.ClassFile;
+import javassist.bytecode.FieldInfo;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -14,6 +22,7 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.apache.maven.plugins.annotations.Parameter;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -52,26 +61,62 @@ public class MojoExecutorBuilder {
 				String mojoClassName = getValue(children, "implementation");
 				System.out.println("  mojo: " + goal + "[" + mojoClassName
 						+ "]");
-				for (Method m : getMethods(mojoClassName)) {
-					if (m.getName().startsWith("get")
-							&& !m.getName().equals("getClass")) {
-						System.out.println("    method: " + m.getName());
-						for (Annotation a : m.getAnnotations())
+
+				Parameter p;
+				Class<?> cls = getCls(mojoClassName);
+				while (cls != null) {
+					System.out.println("    class: " + cls);
+					for (Field f : cls.getDeclaredFields()) {
+						// f.setAccessible(true);
+						System.out.println("      field: " + f.getName());
+						for (Annotation a : f.getDeclaredAnnotations())
 							System.out.println("      annotation: "
 									+ a.toString());
 					}
+					cls = cls.getSuperclass();
 				}
+				Class<?> c = getCls(mojoClassName);
+				URL location = c.getResource('/'
+						+ c.getName().replace('.', '/') + ".class");
+				DataInputStream dstream = new DataInputStream(
+						new BufferedInputStream(location.openStream()));
+
+				ClassFile cf = new ClassFile(dstream);
+				for (FieldInfo fi : (List<FieldInfo>) cf.getFields()) {
+					System.out.println(fi);
+					AnnotationsAttribute visible = (AnnotationsAttribute) fi
+							.getAttribute(AnnotationsAttribute.visibleTag);
+					AnnotationsAttribute invisible = (AnnotationsAttribute) fi
+							.getAttribute(AnnotationsAttribute.invisibleTag);
+					if (invisible != null)
+						for (javassist.bytecode.annotation.Annotation ann : invisible
+								.getAnnotations()) {
+							System.out.println("@" + ann.getTypeName());
+						}
+				}
+
 			}
 		} catch (XPathExpressionException e) {
 			throw new RuntimeException(e);
 		} catch (SecurityException e) {
 			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	private Class<?> getCls(String cls) {
+		try {
+			return Class.forName(cls);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
 		}
 	}
 
-	private Method[] getMethods(String mojoClassName) {
+	private Field[] getFields(String mojoClassName) {
 		try {
-			return Class.forName(mojoClassName).getMethods();
+			return Class.forName(mojoClassName).getFields();
 		} catch (SecurityException e) {
 			throw new RuntimeException(e);
 		} catch (ClassNotFoundException e) {
